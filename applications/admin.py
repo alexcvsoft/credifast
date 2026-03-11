10d2a455eef7f6d6b2b5ff71cd54f52a2e8e448d
alexcvsoft <37186929@uaz.edu.mx>
2026-03-11 03:36:38 -0600

    Register models in admin for Application, AddressProof, Decision, and RuleLog

--- applications/admin.py
+++ applications/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Application
+from .models import AddressProof
+from .models import Decision
+from .models import RuleLog
+
+
+admin.site.register(Application)
+admin.site.register(AddressProof)
+admin.site.register(Decision)
+admin.site.register(RuleLog)
