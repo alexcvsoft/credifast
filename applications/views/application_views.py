5889d88c9e47454147881f6864758a2c47837f26
alexcvsoft <37186929@uaz.edu.mx>
2026-03-11 03:42:21 -0600

    Add CreateApplicationView for handling application submissions

---
+++ applications/views/application_views.py
@@ -0,0 +1,24 @@
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from applications.models import Application
+from applications.serializers.application_serializer import ApplicationSerializer
+
+
+class CreateApplicationView(APIView):
+
+    def post(self, request):
+
+        serializer = ApplicationSerializer(data=request.data)
+
+        if serializer.is_valid():
+
+            application = serializer.save()
+
+            return Response(
+                ApplicationSerializer(application).data,
+                status=status.HTTP_201_CREATED
+            )
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
